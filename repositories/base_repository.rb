055af6aadd163e8304cdf8b46d6baf9278aa7dc0
Tomasz Jaśkiewicz <tomasz.jaskiewicz@netguru.pl>
2016-06-24 15:09:06 +0200

    Extract code to BaseRepository

--- repositories/base_repository.rb
+++ repositories/base_repository.rb
@@ -1,2 +1,19 @@
 class BaseRepository
-end 
+  def write(row)
+    CSV.open(path, "ab") { |csv| csv << row }
+  end
+
+  def all
+    array_of_records.map { |row| Object.const_get("::#{classname}").new(row) }
+  end
+
+  def find_by_id(id)
+    all.find { |record| record.id.to_s == id.to_s }
+  end
+
+  private
+
+  def array_of_records
+    CSV.read(path)
+  end
+end
