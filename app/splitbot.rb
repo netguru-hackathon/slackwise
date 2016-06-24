e751090785b045cb8489c9028a7678cda35be685
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2016-06-24 17:20:26 +0200

    Magical

--- app/splitbot.rb
+++ app/splitbot.rb
@@ -1,8 +1,24 @@
 require 'slack-ruby-bot'
+require_relative './context'
+require_relative './command_parser'
+require_relative '../app/repositories/users_repository'
 
 class Splitbot < SlackRubyBot::Bot
   command 'ping' do |client, data, match|
     client.say(text: 'pong', channel: data.channel)
+  end
+
+  command 'add_expense' do |client, data, match|
+    current_user = UsersRepository.new.find_by_id(data[:user])
+    context = Slackwise::Context.new(current_user)
+    command_parser = Slackwise::CommandParser.new(context)
+
+    command = data[:text]
+
+    command_words = command.split(' ')
+    command_parser.public_send(command_words[0], command_words[1..-1])
+
+    client.say(text: 'Expense sucesfully added', channel: data.channel)
   end
 end
 
